cf740028770769d6aff6ab2622b9dd30a4160c71
Ruthy Hellen Dias <142604700+R2618@users.noreply.github.com>
2024-12-03 07:54:48 -0300

    alegriaaaaa

--- Ajudinha_SAEP/paginas/cadastroTarefas.php
+++ Ajudinha_SAEP/paginas/cadastroTarefas.php
@@ -15,6 +15,21 @@
         <div>
             <textarea id="story" name="exercicio" rows="10" cols="33" placeholder="Exercício: "></textarea>
         </div>
+        <br> 
+
+        <label>Turmas</label>
+        <select name="turmas">
+            <option value="turma1" name="tur1">Turma1</option>
+            <option value="turma2" name="tur2">Turma2</option>
+            <option value="turma3" name="tur3">Turma3</option>
+            <option value="turma4" name="tur4">Turma4</option>
+            <option value="turma5" name="tur5">Turma5</option>
+            <option value="turma6" name="tur6">Turma6</option>
+        </select>
+
+        <br>
+        <div class=""><input type="submit" value="Enviar"class="botao"></div>
+          
     </form>
 
 </body>
--- Ajudinha_SAEP/php/cadastroTarefas.php
+++ Ajudinha_SAEP/php/cadastroTarefas.php
@@ -4,11 +4,21 @@ session_start();
 
 $descricao = $conn->real_escape_string($_POST['descricao']);
 $exercicio= $conn->real_escape_string($_POST['exercicio']);
+$tur1= $conn->real_escape_string($_POST['tur1']);
+$tur2= $conn->real_escape_string($_POST['tur2']);
+$tur3= $conn->real_escape_string($_POST['tur3']);
+$tur4= $conn->real_escape_string($_POST['tur4']);
+$tur5= $conn->real_escape_string($_POST['tur5']);
+$tur6= $conn->real_escape_string($_POST['tur6']);
 
-$sql = "INSERT INTO usuario
-(descricao,exercicio) 
+
+$sql = "INSERT INTO turmas
+(descricao,exercicio,tur1,tur2,tur3,tur4,tur5,tur6) 
 VALUES 
-('" . $descricao . "','" . $exercicio . "')";
+('" . $descricao . "','" . $exercicio . "','" . $tur1 . "','" . $tur2 . "','" . $tur3 . "','" . $tur4 . "','" . $tur5 . "','" . $tur6 . "')";
 
 
 $resultado = $conn->query($sql);
+$conn->close();
+header('location:../paginas/cadastroTarefas.php', true, 301);
+exit();
